329414a77d590dca8cfe8e57886272fefbeb39ac
Sariells <wwqkere@mail.ru>
2026-09-01 09:26:40 +0500

    i mogged this code

---
+++ src/map/Map.cpp
@@ -0,0 +1,12 @@
+//
+// Created by user on 01.09.2026.
+//
+
+#include "Map.h"
+
+Map::Map(int x, int y)
+:   Width(x),
+    Height(y)
+{
+    map.resize(x * y);
+}
---
+++ src/map/Map.h
@@ -0,0 +1,22 @@
+//
+// Created by user on 01.09.2026.
+//
+
+#ifndef RPGMAKER_MAP_H
+#define RPGMAKER_MAP_H
+#include <vector>
+
+class Map {
+public:
+    Map(int x, int y);
+    //Это наш размер карты будет
+    int Width = 0;
+    int Height = 0;
+    //это массив он будет заполнен  кол-во элементов по формуле widht * height = elements
+    std::vector<int> map;
+    //а потом мы сделаем что-то вроде
+
+};
+
+
+#endif //RPGMAKER_MAP_H
